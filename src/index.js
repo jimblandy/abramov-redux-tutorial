96cca9cf557e994382dc8a9f8dafef2a2a463d3a
Jim Blandy <jimb@red-bean.com>
2018-04-03 11:10:50 -0700

    Extracting Container Components (VisibleTodoList, AddTodo)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,80 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { todoApp } from './todos';
 
+const AddTodo = () => {
+  let input;
+  return (
+    <div>
+      <input ref={node => { input = node; }} />
+      <button onClick={() => {
+          store.dispatch({ type: 'ADD_TODO', id: nextTodoId++, text: input.value });
+          input.value = '';
+      }}>
+        Add Todo
+      </button>
+    </div>
+  );
+};
+
+const Todo = ({ onClick, completed, text }) => (
+  <li
+    onClick={onClick}
+    style={{ textDecoration: completed ? 'line-through' : 'none' }}>
+    {text}
+  </li>
+);
+
+const TodoList = ({ todos, onTodoClick }) => (
+  <ul>
+    {todos.map(todo => (
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+        />
+    ))}
+  </ul>
+);
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+  case 'SHOW_ALL':
+    return todos;
+  case 'SHOW_COMPLETED':
+    return todos.filter(todo => todo.completed);
+  case 'SHOW_ACTIVE':
+    return todos.filter(todo => !todo.completed);
+  default:
+    console.log('unexpected visibility filter: ' + filter);
+    return todos;
+  }
+};
+
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() =>
+      this.forceUpdate()
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const state = store.getState();
+
+    return (
+      <TodoList
+        todos={getVisibleTodos(state.todos, state.visibilityFilter)}
+        onTodoClick={ id => {
+          store.dispatch({ type: 'TOGGLE_TODO', id });
+        }}
+      />
+    );
+  }
+}
+
 const Link = ({ active, onClick, children }) => {
   if (active)
     return <span>{children}</span>;
@@ -57,83 +131,18 @@ const Footer = () => (
   </p>
 );
 
-const Todo = ({ onClick, completed, text }) => (
-  <li
-    onClick={onClick}
-    style={{ textDecoration: completed ? 'line-through' : 'none' }}>
-    {text}
-  </li>
-);
-
-const TodoList = ({ todos, onTodoClick }) => (
-  <ul>
-    {todos.map(todo => (
-      <Todo
-        key={todo.id}
-        {...todo}
-        onClick={() => onTodoClick(todo.id)}
-        />
-    ))}
-  </ul>
-);
-
-const AddTodo = ({ onAddClick }) => {
-  let input;
-  return (
-    <div>
-      <input ref={node => { input = node; }} />
-      <button onClick={() => {
-          onAddClick(input.value);
-          input.value = '';
-        }}>
-        Add Todo
-      </button>
-    </div>
-  );
-};
-
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-  case 'SHOW_ALL':
-    return todos;
-  case 'SHOW_COMPLETED':
-    return todos.filter(todo => todo.completed);
-  case 'SHOW_ACTIVE':
-    return todos.filter(todo => !todo.completed);
-  default:
-    console.log('unexpected visibility filter: ' + filter);
-    return todos;
-  }
-};
-
 let nextTodoId = 0;
-const TodoApp = ({ todos, visibilityFilter }) => (
+const TodoApp = () => (
   <div>
-    <AddTodo
-      onAddClick={text => {
-        store.dispatch({ type: 'ADD_TODO', text, id: nextTodoId++ });
-      }}
-      />
-    <TodoList
-      todos={getVisibleTodos(todos, visibilityFilter)}
-      onTodoClick={id => {
-        store.dispatch({ type: 'TOGGLE_TODO', id });
-      }}
-      />
+    <AddTodo />
+    <VisibleTodoList />
     <Footer />
   </div>
 );
 
 const store = createStore(todoApp);
 
-const render = () => {
-  ReactDOM.render(
-    <TodoApp
-      {...store.getState()}
-    />,
-    document.getElementById('root')
-  )
-};
-
-store.subscribe(render);
-render();
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);
